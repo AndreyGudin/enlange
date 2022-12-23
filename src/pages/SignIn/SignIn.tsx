79fdf4c6166a5c94a93f1826bcbb97926e33db7f
Andrey Gudin <aeon150791@gmail.com>
2022-12-23 14:37:47 +0400

    refactor: change some titles to russian

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -11,8 +11,8 @@ export const SignIn = () => {
   } = useForm<User>({ mode: 'all' });
   return (
     <section className="h-screen">
-      <div className="flex px-6 py-12 h-full flex-col justify-center items-center">
-        <div className="flex w-8/12 flex-col justify-center items-center flex-wrap g-6 text-gray-800 bg-indigo-500 h-auto py-4 rounded-2xl">
+      <div className="flex px-6 py-12 h-full justify-center items-center">
+        <div className="flex w-8/12 justify-center items-center flex-wrap g-6 text-gray-800 bg-indigo-500 h-auto py-4 rounded-2xl">
           <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
             <img
               src={img}
@@ -22,7 +22,6 @@ export const SignIn = () => {
           </div>
           <div className="md:w-8/12 lg:w-5/12">
             <form className="flex flex-col items-center w-full">
-              {/* <!-- Email input --> */}
               <div className="mb-6 w-full">
                 <input
                   type="text"
@@ -30,8 +29,6 @@ export const SignIn = () => {
                   placeholder="Email address"
                 />
               </div>
-
-              {/* <!-- Password input --> */}
               <div className="mb-6 w-full">
                 <input
                   type="password"
@@ -52,18 +49,16 @@ export const SignIn = () => {
                     className="form-check-label inline-block text-gray-800"
                     htmlFor="exampleCheck2"
                   >
-                    Remember me
+                    Запомнить меня
                   </label>
                 </div>
                 <a
                   href="#!"
                   className="text-black hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
                 >
-                  Forgot password?
+                  Забыли пароль?
                 </a>
               </div>
-
-              {/* <!-- Submit button --> */}
               <button
                 type="submit"
                 className="inline-block px-7 py-3 bg-indigo-900 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
