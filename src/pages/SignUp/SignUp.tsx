1dd42860c9045365db26a75f8104506da9ae6cfa
Andrey Gudin <aeon150791@gmail.com>
2022-12-26 10:46:15 +0400

    feat: add style for disabled button

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -83,7 +83,7 @@ export const SignUp = () => {
               </div>
               <button
                 type="submit"
-                className="inline-block px-7 py-3 bg-indigo-900 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+                className="inline-block px-7 py-3 bg-indigo-900 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full disabled:bg-slate-500 disabled:hover:shadow-md disabled:cursor-not-allowed"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
                 disabled={!isValid}
