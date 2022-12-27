c5d8c0a340791e880df68b4e26c702ac458b041a
Andrey Gudin <aeon150791@gmail.com>
2022-12-27 14:33:49 +0400

    feat: add style for user buttons

--- src/components/Header/Header.user.tsx
+++ src/components/Header/Header.user.tsx
@@ -2,11 +2,11 @@
 
 export const SignedInUserButtons = () => { 
   return (
-    <div>
-      <button>
+    <div className="flex gap-3">
+      <button className="text-white w-[137px] h-[52px] bg-purple-600 rounded hover:bg-purple-500 duration-300">
         Пользователь
       </button>
-      <button>
+      <button className="hover:underline">
         Выйти
       </button>
     </div>
