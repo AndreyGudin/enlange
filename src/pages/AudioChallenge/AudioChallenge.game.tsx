17db9e07e4182dd634d11297a705580f4cb199fc
Andrey Gudin <aeon150791@gmail.com>
2023-01-09 13:46:46 +0400

    feat: add auido play for word

--- src/pages/AudioChallenge/AudioChallenge.game.tsx
+++ src/pages/AudioChallenge/AudioChallenge.game.tsx
@@ -1,13 +1,17 @@
-import { MouseEventHandler, useEffect, useState } from 'react';
+import { MouseEventHandler, useEffect, useRef, useState } from 'react';
 import { useWordsForAudioChallengeGame } from '../../hooks/useWordsForAudioChallengeGame';
-import { getRandomPagesFromGroup } from '../../services/api';
+import { ApiLinks, getRandomPagesFromGroup } from '../../services/api';
 import { Word } from '../../types/types';
 
 export const AudioChallengeGame = () => {
   const { answers, choices, loading } = useWordsForAudioChallengeGame();
   const [next, setNext] = useState(0);
   const [clicked, setClicked] = useState('');
-
+  const refAudio = useRef<HTMLAudioElement>(null);
+  
+  useEffect(() => {
+    refAudio.current?.play();
+  },[next]);
   const handleNext = () => {
     if (next < 19) {
       console.log(next);
@@ -21,7 +25,10 @@ export const AudioChallengeGame = () => {
 
   return (
     <div className="flex flex-col gap-5">
-      <div>{loading ? null : answers[next].word}</div>
+      <div>
+        <div>{loading ? null : <audio autoPlay ref={refAudio} src={`${ApiLinks.Link}/${answers[next].audio}`}></audio>}</div>
+        <div>{loading ? null : answers[next].word}</div>
+      </div>
       <div className="flex gap-3">
         {loading
           ? null
