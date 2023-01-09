07b0578186db501b33b97e7ad7d600cebf94eea0
Andrey Gudin <aeon150791@gmail.com>
2023-01-09 14:13:29 +0400

    feat: add showing of image and answer word only after answering, add button dont know

--- src/pages/AudioChallenge/AudioChallenge.game.tsx
+++ src/pages/AudioChallenge/AudioChallenge.game.tsx
@@ -8,11 +8,19 @@ export const AudioChallengeGame = () => {
   const [next, setNext] = useState(0);
   const [clicked, setClicked] = useState('');
   const refAudio = useRef<HTMLAudioElement>(null);
-  
+  const [answered, setAnswered] = useState(false);
   useEffect(() => {
     refAudio.current?.play();
-  },[next]);
+  }, [next]);
   const handleNext = () => {
+    if (next < 19) {
+      console.log(next);
+      setNext((v) => (v += 1));
+    }
+    setAnswered(false);
+  };
+
+  const handleDontKnow = () => {
     if (next < 19) {
       console.log(next);
       setNext((v) => (v += 1));
@@ -21,13 +29,19 @@ export const AudioChallengeGame = () => {
 
   const handleClick = (id: string) => {
     setClicked(id);
+    setAnswered(true);
   };
 
   return (
     <div className="flex flex-col gap-5">
       <div>
-        <div>{loading ? null : <audio autoPlay ref={refAudio} src={`${ApiLinks.Link}/${answers[next].audio}`}></audio>}</div>
-        <div>{loading ? null : answers[next].word}</div>
+        <div>
+          {loading ? null : (
+            <audio autoPlay ref={refAudio} src={`${ApiLinks.Link}/${answers[next].audio}`}></audio>
+          )}
+        </div>
+        <div>{loading || !answered ? null : <img src={`${ApiLinks.Link}/${answers[next].image}`} alt="" />}</div>
+        <div>{loading || !answered ? null : answers[next].word}</div>
       </div>
       <div className="flex gap-3">
         {loading
@@ -46,12 +60,21 @@ export const AudioChallengeGame = () => {
               );
             })}
       </div>
-      <button
-        onClick={handleNext}
-        className="text-white w-[137px] h-[52px] bg-purple-600 rounded hover:bg-purple-500 duration-300"
-      >
-        Следующий
-      </button>
+      {answered ? (
+        <button
+          onClick={handleNext}
+          className="text-white w-[137px] h-[52px] cursor-pointer bg-purple-600 rounded hover:bg-purple-500 duration-300"
+        >
+          Следующий
+        </button>
+      ) : (
+        <button
+          onClick={handleDontKnow}
+          className="text-white w-[137px] h-[52px] bg-slate-400 opacity-40 rounded hover:bg-purple-500 hover:opacity-100 duration-300"
+        >
+          Не знаю
+        </button>
+      )}
     </div>
   );
 };
