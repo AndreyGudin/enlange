1db389a7255e2d096912cda9cf037166254f2655
Andrey Gudin <aeon150791@gmail.com>
2023-01-09 11:45:17 +0400

    feat: add select for choices, only style

--- src/pages/AudioChallenge/AudioChallenge.game.tsx
+++ src/pages/AudioChallenge/AudioChallenge.game.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { MouseEventHandler, useEffect, useState } from 'react';
 import { useWordsForAudioChallengeGame } from '../../hooks/useWordsForAudioChallengeGame';
 import { getRandomPagesFromGroup } from '../../services/api';
 import { Word } from '../../types/types';
@@ -6,10 +6,18 @@ import { Word } from '../../types/types';
 export const AudioChallengeGame = () => {
   const { answers, choices, loading } = useWordsForAudioChallengeGame();
   const [next, setNext] = useState(0);
+  const [clicked, setClicked] = useState('');
 
-  const handleClick = () => {
-    if (next < 20) setNext(v => v += 1);
-  }
+  const handleNext = () => {
+    if (next < 19) {
+      console.log(next);
+      setNext((v) => (v += 1));
+    }
+  };
+
+  const handleClick = (id: string) => {
+    setClicked(id);
+  };
 
   return (
     <div className="flex flex-col gap-5">
@@ -18,10 +26,23 @@ export const AudioChallengeGame = () => {
         {loading
           ? null
           : choices[next].map((word) => {
-              return <div key={word.id}>{word.wordTranslate}</div>;
+              return (
+                <div
+                  key={word.id}
+                  onClick={() => handleClick(word.id)}
+                  className={`${
+                    clicked === word.id ? 'bg-violet-600' : ''
+                  } p-2 border rounded hover:bg-violet-600 hover:cursor-pointer`}
+                >
+                  {word.wordTranslate}
+                </div>
+              );
             })}
       </div>
-      <button onClick={handleClick} className="text-white w-[137px] h-[52px] bg-purple-600 rounded hover:bg-purple-500 duration-300">
+      <button
+        onClick={handleNext}
+        className="text-white w-[137px] h-[52px] bg-purple-600 rounded hover:bg-purple-500 duration-300"
+      >
         Следующий
       </button>
     </div>
